33e3027d3aab015ad16dbc9f3e9ccdfea8b6e1d9
Nkfree <BlackVaclav@gmail.com>
2021-07-15 11:32:29 +0200

    Added align with option + minor tweaks
    - selected object can be aligned with another
    - last align operation can be undone in case X and Y axis were mixed up

--- 0.7/decorateHelp.lua
+++ 0.7/decorateHelp.lua
@@ -1,5 +1,7 @@
 -- decorateHelp - Release 5 custom - For tes3mp v0.7.0-alpha
 -- To be used with kanaFurniture release 5 custom that features minor QoL changes and selected object highlighting
+-- Alter positions of items using a GUI
+-- Added option to align selected object with another
 
 --[[ INSTALLATION:
 1) Save this file as "decorateHelp.lua" in server/scripts/custom
@@ -9,35 +11,32 @@
 ------
 local config = {}
 
-config.MainId = 31360
-config.PromptId = 31361
+config.MainId = 31359
+config.PromptId = 31360
 config.ScaleMin = 0.5
 config.ScaleMax = 2.0
+
+config.AlignId = 31361
+config.ChooseAlignObjectId = 31362
+
 ------
 
-Methods = {}
+local Methods = {}
 
 tableHelper = require("tableHelper")
 
 --
-local playerSelectedObject = {}
+Methods.playerSelectedObject = {}
 local playerCurrentMode = {}
 
---Returns the object's data from a loaded cell. Doesn't need to load the cell because this assumes it'll always be called in a cell that's loaded.
-local function getObject(refIndex, cell)
-	if refIndex == nil then
-		return false
-	end
+local playerAlignOptions = {}
+Methods.playerSelectedAlignObject = {}
 
-	if LoadedCells[cell]:ContainsObject(refIndex) then
-		return LoadedCells[cell].data.objectData[refIndex]
-	else
-		return false
-	end	
-end
+local playerActionHistory = {}
+--
 
 local function resendPlaceToAll(refIndex, cell)
-	local object = getObject(refIndex, cell)
+	local object = kanaFurniture.getObject(refIndex, cell)
 	
 	if not object then
 		return false
@@ -102,6 +101,110 @@ local function resendPlaceToAll(refIndex, cell)
 	LoadedCells[cell]:QuicksaveToDrive() --Not needed, but it's nice to do anyways
 end
 
+--Align functions
+local function OnPlayerAuthentifiedHandler(pid)
+	if not playerActionHistory[pid] then playerActionHistory[pid] = {} end
+end
+
+local function chooseAlignObjectGUI(pid)
+	local pname = tes3mp.GetName(pid)
+	
+	local cell = tes3mp.GetCell(pid)
+	local options = kanaFurniture.PlacedInCell(pname, cell)
+	
+	local list = "* CLOSE *\n"
+	local newOptions = {}
+	
+	if options and #options > 0 then
+		for i = 1, #options do
+			--Make sure the object still exists, and get its data
+			local object = kanaFurniture.getObject(options[i], cell)
+			
+			if object and options[i] ~= Methods.playerSelectedObject[pname] then -- Object exists and is not selected
+				local furnData = kanaFurniture.FurnitureData(object.refId)
+				
+				list = list .. furnData.name .. " (at " .. math.floor(object.location.posX + 0.5) .. ", "  ..  math.floor(object.location.posY + 0.5) .. ", " .. math.floor(object.location.posZ + 0.5) .. ")"
+				if not(i == #options) then
+					list = list .. "\n"
+				end
+				
+				table.insert(newOptions, {refIndex = options[i], refId = object.refId})
+			end
+		end
+	end
+	
+	playerAlignOptions[pname] = newOptions
+	tes3mp.ListBox(pid, config.ChooseAlignObjectId, "Choose a piece of furniture to align selected object with.", list)
+end
+
+local function onChooseAlignObject(pid, loc)
+	local pname = tes3mp.GetName(pid)
+	local cell = tes3mp.GetCell(pid)
+	Methods.playerSelectedAlignObject[pname] = playerAlignOptions[pname][loc].refIndex
+	
+	kanaFurniture.OnStartHighlight(pid, cell, Methods.playerSelectedAlignObject[pname])
+end
+
+local function showAlignGUI(pid)
+	local cell = tes3mp.GetCell(pid)
+	local pname = tes3mp.GetName(pid)
+	
+	local selectedObject = kanaFurniture.getObject(Methods.playerSelectedObject[pname], cell)
+	local alignToObject = kanaFurniture.getObject(Methods.playerSelectedAlignObject[pname], cell)
+	
+	if selectedObject and alignToObject then --If they have an entry and it isn't gone
+		local flSelectedX = math.floor(selectedObject.location.posX + 0.5)
+		local flSelectedY = math.floor(selectedObject.location.posY + 0.5)
+		local flAlignX = math.floor(alignToObject.location.posX + 0.5)
+		local flAlignY = math.floor(alignToObject.location.posY + 0.5)
+		local message = "Offset X: " .. tostring(math.abs(flSelectedX - flAlignX)) .. "\nOffset Y: " .. tostring(math.abs(flSelectedY - flAlignY))
+		tes3mp.CustomMessageBox(pid, config.AlignId, message, "Align X;Align Y;Align Z;Undo;Close")
+	end
+end
+
+local function onAlign(pid, data)
+	local cell = tes3mp.GetCell(pid)
+	local selectedRefIndex = Methods.GetSelectedRefIndex(pid)
+	local alignRefIndex = Methods.GetSelectedAlignRefIndex(pid)
+	
+	local selectedObject = kanaFurniture.getObject(selectedRefIndex, cell)
+	local alignToObject = kanaFurniture.getObject(alignRefIndex, cell)
+
+	--Temporarily save selected object's location in case of UNDO action
+	playerActionHistory[pid][selectedRefIndex] = {
+		location = {posX = selectedObject.location.posX, posY = selectedObject.location.posY, posZ = selectedObject.location.posZ}
+	}
+	
+	if data == 0 then
+		local alignToPosX = alignToObject.location.posX
+		selectedObject.location.posX = alignToPosX
+	elseif data == 1 then
+		local alignToPosY = alignToObject.location.posY
+		selectedObject.location.posY = alignToPosY
+	elseif data == 2 then
+		local alignToPosZ = alignToObject.location.posZ
+		selectedObject.location.posZ = alignToPosZ
+	end
+	
+	resendPlaceToAll(selectedRefIndex, cell)
+end
+
+local function onAlignUndo(pid)
+	local cell = tes3mp.GetCell(pid)
+	local selectedRefIndex = Methods.GetSelectedRefIndex(pid)
+	local selectedObject = kanaFurniture.getObject(selectedRefIndex, cell)
+
+	if playerActionHistory[pid][selectedRefIndex] then
+		selectedObject.location.posX = playerActionHistory[pid][selectedRefIndex].location.posX
+		selectedObject.location.posY = playerActionHistory[pid][selectedRefIndex].location.posY
+		selectedObject.location.posZ = playerActionHistory[pid][selectedRefIndex].location.posZ
+
+		resendPlaceToAll(selectedRefIndex, cell)
+		playerActionHistory[pid][selectedRefIndex] = nil
+	end
+end
+
+-----------
 
 local function showPromptGUI(pid)
 	local message = "[" .. playerCurrentMode[tes3mp.GetName(pid)] .. "] - Enter a number."
@@ -109,7 +212,7 @@ local function showPromptGUI(pid)
 	local cell = tes3mp.GetCell(pid)
 	
 	if playerCurrentMode[pname] == "Fine Tune Scale" then
-		local object = getObject(playerSelectedObject[pname], cell)
+		local object = kanaFurniture.getObject(Methods.playerSelectedObject[pname], cell)
 		local scale = object.scale or 1
 		tes3mp.InputDialog(pid, config.PromptId, message, "Current scale: " .. scale .. "\nMinimum value: " .. config.ScaleMin .. "\nMaximum value: " .. config.ScaleMax)
 	else
@@ -123,7 +226,7 @@ local function onEnterPrompt(pid, data)
 	local mode = playerCurrentMode[pname]
 	local data = tonumber(data) or 0
 	
-	local object = getObject(playerSelectedObject[pname], cell)
+	local object = kanaFurniture.getObject(Methods.playerSelectedObject[pname], cell)
 	
 	if not object then
 		--The object no longer exists, so we should bail out now
@@ -174,30 +277,32 @@ local function onEnterPrompt(pid, data)
 		if data <= config.ScaleMax and data >= config.ScaleMin then
 			object.scale = data
 		end
+	elseif mode == "Align" then
+		return chooseAlignObjectGUI(pid)
 	elseif mode == "return" then
-		object.location.posY = object.location.posY		
+		object.location.posY = object.location.posY
 		return
 	end
 	
-	resendPlaceToAll(playerSelectedObject[pname], cell)
+	resendPlaceToAll(Methods.playerSelectedObject[pname], cell)
 end
 
 local function showMainGUI(pid)
 	--Determine if the player has an item
 	local currentItem = "None" --default
-	local selected = playerSelectedObject[tes3mp.GetName(pid)]
-	local object = getObject(selected, tes3mp.GetCell(pid))
+	local selected = Methods.playerSelectedObject[tes3mp.GetName(pid)]
+	local object = kanaFurniture.getObject(selected, tes3mp.GetCell(pid))
 	
 	if selected and object then --If they have an entry and it isn't gone
 		currentItem = object.refId .. " (" .. selected .. ")"
 	end
 	
 	local message = "Select an option. Your current item is: " .. currentItem
-	tes3mp.CustomMessageBox(pid, config.MainId, message, "Select Furniture;Fine Tune North;Fine Tune East;Fine Tune Height;Rotate X;Rotate Y;Rotate Z;Raise;Lower;Move East;Move West;Move North;Move South;Scale Up;Scale Down;Fine Tune Scale;Exit")
+	tes3mp.CustomMessageBox(pid, config.MainId, message, "Select Furniture;Fine Tune North;Fine Tune East;Fine Tune Height;Rotate X;Rotate Y;Rotate Z;Raise;Lower;Move East;Move West;Move North;Move South;Scale Up;Scale Down;Fine Tune Scale;Align with;Exit")
 end
 
 local function setSelectedObject(pid, refIndex)
-	playerSelectedObject[tes3mp.GetName(pid)] = refIndex
+	Methods.playerSelectedObject[tes3mp.GetName(pid)] = refIndex
 end
 
 Methods.SetSelectedObject = function(pid, refIndex)
@@ -209,7 +314,7 @@ Methods.OnObjectPlace = function(pid, cellDescription)
 	tes3mp.ReadLastEvent()
 	
 	--Get the refIndex of the first item in the object place packet (in theory, there should only by one)
-	local refIndex = tes3mp.GetObjectRefNumIndex(0) .. "-" .. tes3mp.GetObjectMpNum(0)
+	local refIndex = tes3mp.kanaFurniture.getObjectRefNumIndex(0) .. "-" .. tes3mp.kanaFurniture.getObjectMpNum(0)
 	
 	--Record that item as the last one the player interacted with in this cell
 	setSelectedObject(pid, refIndex)
@@ -221,7 +326,7 @@ Methods.OnGUIAction = function(pid, idGui, data)
 	if idGui == config.MainId then
 		if tonumber(data) == 0 then --View Furniture Emporium
 			playerCurrentMode[pname] = "Select Furniture"
-			kanaFurniture.OnStopHighlight(pid)
+			kanaFurniture.OnStopHighlight(pid, Methods.playerSelectedObject[pname])
 			kanaFurniture.OnView(pid)
 			return true
 		elseif tonumber(data) == 1 then --Move North
@@ -284,9 +389,18 @@ Methods.OnGUIAction = function(pid, idGui, data)
 			playerCurrentMode[pname] = "Fine Tune Scale"
 			showPromptGUI(pid)
 			return true
-		elseif tonumber(data) == 16 then --Close
+		elseif tonumber(data) == 16 then -- Align
+			--Exit if no object selected prior to this menu
+			if not Methods.GetSelectedRefIndex(pid) then
+				showMainGUI(pid)
+			else
+				playerCurrentMode[pname] = "Align"
+				onEnterPrompt(pid, 0)
+			end
+			return true 
+		elseif tonumber(data) == 17 then --Exit
 			--Do nothing
-			kanaFurniture.OnStopHighlight(pid)
+			kanaFurniture.OnStopHighlight(pid, Methods.playerSelectedObject[pname])
 			return true
 		end
 	elseif idGui == config.PromptId then
@@ -296,28 +410,64 @@ Methods.OnGUIAction = function(pid, idGui, data)
 		
 		playerCurrentMode[tes3mp.GetName(pid)] = nil
 		return true, showMainGUI(pid)
+	elseif idGui == config.ChooseAlignObjectId then
+		if tonumber(data) == 0 or tonumber(data) == 18446744073709551615 then --Close/Nothing Selected
+			showMainGUI(pid)
+			return true
+		else
+			onChooseAlignObject(pid, tonumber(data))
+			showAlignGUI(pid)
+			return true
+		end
+	elseif idGui == config.AlignId then
+		if tonumber(data) == 3 then
+			onAlignUndo(pid)
+			showAlignGUI(pid)
+			return true
+		elseif tonumber(data) == 4 then
+			kanaFurniture.OnStopHighlight(pid, Methods.playerSelectedAlignObject[pname])
+			Methods.playerSelectedAlignObject[pname] = nil
+			showMainGUI(pid)
+			return true
+		else
+			onAlign(pid, tonumber(data))
+			showAlignGUI(pid)
+			return true
+		end
 	end
 end
 
 Methods.OnPlayerCellChange = function(pid)
-	playerSelectedObject[tes3mp.GetName(pid)] = nil
+	Methods.playerSelectedObject[tes3mp.GetName(pid)] = nil
+	Methods.playerSelectedAlignObject[tes3mp.GetName(pid)] = nil
 end
 
 Methods.OnCommand = function(pid)
 	local cell = tes3mp.GetCell(pid)
-	local pname = tes3mp.GetName(pid)
-	local refIndex = playerSelectedObject[pname]
-	
-	if getObject(refIndex, cell) then
+	local refIndex = Methods.GetSelectedRefIndex(pid)
+	if refIndex then
 		kanaFurniture.OnStartHighlight(pid, cell, refIndex)
 	end
-	
 	showMainGUI(pid)
+end
+
+Methods.GetSelectedRefIndex = function(pid)
+	local pname = tes3mp.GetName(pid)
+	return Methods.playerSelectedObject[pname]
+end
+
+Methods.GetSelectedAlignRefIndex = function(pid)
+	local pname = tes3mp.GetName(pid)
+	return Methods.playerSelectedAlignObject[pname]
 end
 
 customCommandHooks.registerCommand("decorator", function(pid, cmd) decorateHelp.OnCommand(pid) end)
 customCommandHooks.registerCommand("decorate", function(pid, cmd) decorateHelp.OnCommand(pid) end)
 customCommandHooks.registerCommand("dh", function(pid, cmd) decorateHelp.OnCommand(pid) end)
+
+customEventHooks.registerHandler("OnPlayerAuthentified", function(eventStatus, pid)
+	OnPlayerAuthentifiedHandler(pid)
+end)
 
 customEventHooks.registerHandler("OnGUIAction", function(eventStatus, pid, idGui, data)
 	decorateHelp.OnGUIAction(pid, idGui, data)
